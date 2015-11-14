50aaee82a41d15bbc284170f6c10c52eee791752
Adam Sokolnicki <adam.sokolnicki@gmail.com>
2015-11-14 01:16:20 +0100

    Fix the automatic inclusion of the matchers

--- lib/drama/rspec.rb
+++ lib/drama/rspec.rb
@@ -1,7 +1,7 @@
 if defined?(RSpec)
   require 'drama/rspec/act_example_group'
   require 'drama/rspec/matchers'
-  RSpec.configuration.include(Drama::RSpec::ActExampleGroup,                type: :act)
-  RSpec.configuration.include(Drama::RSpec::Matchers::RequireParamsMatcher, type: :act)
-  RSpec.configuration.include(Drama::RSpec::Matchers::ActOnMatcher, type: :controller)
+  RSpec.configuration.include(Drama::RSpec::ActExampleGroup,         type: :act)
+  RSpec.configuration.include(Drama::RSpec::Matchers::RequireParams, type: :act)
+  RSpec.configuration.include(Drama::RSpec::Matchers::ActOn,         type: :controller)
 end
